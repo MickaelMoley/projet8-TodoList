09347f4b520d2ffb0f6e689f13dd22f2de6f9320
Mickael Moley <mickael.moley@outlook.com>
2022-02-10 21:05:00 +0100

    Fix #9 Admin can delete task anonymous user && fix role hierarchy

--- src/AppBundle/Security/Voter/TaskVoter.php
+++ src/AppBundle/Security/Voter/TaskVoter.php
@@ -5,12 +5,19 @@ namespace AppBundle\Security\Voter;
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class TaskVoter extends Voter
 {
+    private $authorizationChecker;
+
     const TASK_DELETE = 'task_delete';
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker){
+        $this->authorizationChecker = $authorizationChecker;
+    }
 
     protected function supports(string $attribute, $subject): bool
     {
@@ -32,6 +39,12 @@ class TaskVoter extends Voter
             return false;
         }
 
+        /*On vérifie si c'est l'utilisateur avec le rôle "ROLE_ADMIN" qui souhaite supprimer une tâche
+            Admins can delete tasks, even tasks with anonymous user
+        */
+        if($this->authorizationChecker->isGranted('ROLE_ADMIN')){
+            return true;
+        }
 
         switch ($attribute) {
             case self::TASK_DELETE:
