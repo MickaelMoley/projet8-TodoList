9f9c055afa96a20973fbe703a8c850930a600896
Mickael Moley <mickael.moley@outlook.com>
2022-02-18 12:56:57 +0100

    Test if homepage is redirect to login page

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,17 +2,22 @@
 
 namespace Tests\AppBundle\Controller;
 
+use http\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @runInSeparateProcess
+     */
     public function testIndex()
     {
+        /** @var Client $client */
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
     }
 }
