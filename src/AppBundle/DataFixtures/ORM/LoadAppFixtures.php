161e321171b2cea6c156f56c857f170243042a71
Mickael Moley <mickael.moley@outlook.com>
2022-03-04 12:12:29 +0100

    Add fixtures for Tasks
    Create tasks for user

--- src/AppBundle/DataFixtures/ORM/LoadAppFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadAppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
@@ -12,6 +13,15 @@ class LoadAppFixtures implements FixtureInterface {
 
 
 	private UserPasswordHasherInterface $passwordHasher;
+	private ObjectManager $manager;
+
+
+	const NB_TASKS_PER_USERS = 10;
+
+	public $guestUser = null;
+	public $adminUser = null;
+	public $anonymousUser = null;
+
 
 	public function __construct(UserPasswordHasherInterface $passwordHasher)
 	{
@@ -24,9 +34,11 @@ class LoadAppFixtures implements FixtureInterface {
 	 */
 	public function load(ObjectManager $manager)
 	{
+		$this->manager = $manager;
 		$this->createGuestUser($manager);
 		$this->createAdminUser($manager);
 		$this->createAnonymousUser($manager);
+		$this->createTasks();
 	}
 
 	public function createGuestUser(ObjectManager $manager){
@@ -39,6 +51,7 @@ class LoadAppFixtures implements FixtureInterface {
 
 		$manager->persist($user);
 		$manager->flush();
+		$this->guestUser = $user;
 	}
 
 	private function createAdminUser(ObjectManager $manager)
@@ -51,6 +64,7 @@ class LoadAppFixtures implements FixtureInterface {
 
 		$manager->persist($user);
 		$manager->flush();
+		$this->adminUser = $user;
 	}
 
 	private function createAnonymousUser(ObjectManager $manager)
@@ -63,5 +77,73 @@ class LoadAppFixtures implements FixtureInterface {
 
 		$manager->persist($user);
 		$manager->flush();
+		$this->anonymousUser = $user;
+	}
+
+	private function createTasks()
+	{
+		
+		$this->createTasksForGuestUser();
+		$this->createTasksForAdminUser();
+		$this->createTasksForAnonymousUser();
+
+	}
+
+	/**
+	 * Créer des tâches pour l'utilisateur simple
+	 * @return void
+	 */
+	private function createTasksForGuestUser()
+	{
+
+		for ($i = 0; $i < self::NB_TASKS_PER_USERS; $i++){
+
+			$task = new Task();
+			$task->setTitle(sprintf("Task_#%s", $i));
+			$task->setContent(sprintf('Super contenu'));
+			$task->setCreatedAt(new \DateTime());
+			$task->setUser($this->guestUser);
+			$this->manager->persist($task);
+		}
+
+		$this->manager->flush();
+	}
+
+	/**
+	 * Créer des tâches pour l'utilisateur Admin
+	 * @return void
+	 */
+	private function createTasksForAdminUser()
+	{
+		for ($i = 0; $i < self::NB_TASKS_PER_USERS; $i++){
+
+			$task = new Task();
+			$task->setTitle(sprintf("Task_#%s", $i));
+			$task->setContent(sprintf('Super contenu'));
+			$task->setCreatedAt(new \DateTime());
+			$task->setUser($this->adminUser);
+			$this->manager->persist($task);
+		}
+
+		$this->manager->flush();
+	}
+
+	/**
+	 * Créer des tâches pour l'utilisateur anonyme
+	 * @return void
+	 */
+	private function createTasksForAnonymousUser()
+	{
+		for ($i = 0; $i < self::NB_TASKS_PER_USERS; $i++){
+
+			$task = new Task();
+			$task->setTitle(sprintf("Task_#%s", $i));
+			$task->setContent(sprintf('Super contenu'));
+			$task->setCreatedAt(new \DateTime());
+			$task->setUser($this->anonymousUser);
+			$this->manager->persist($task);
+		}
+
+		$this->manager->flush();
 	}
 }
