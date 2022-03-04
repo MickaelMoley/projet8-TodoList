ff270398ccac64ac1b7e80e9735df6baf8a2d417
Mickael Moley <mickael.moley@outlook.com>
2022-03-04 12:13:11 +0100

    Add fonctional test for delete task

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -3,6 +3,9 @@
 namespace Tests\AppBundle\Controller;
 
 use App\Tests\AppBundle\BaseTest;
+use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use http\Client;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
@@ -45,7 +48,39 @@ class TaskControllerTest extends BaseTest
 
 		$this->assertSelectorExists('div:contains("Superbe ! La tâche a été bien été ajoutée.")');
 
+	}
 
+	/**
+	 * Test fonctionnel permettant de créer une tâche et supprimer sa propre tâche
+	 * @runInSeparateProcess
+	 */
+	public function testDeleteOwnTask(){
+
+		/** @var EntityManagerInterface $entityManager */
+		$entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+		$this->client->request('GET', '/');
+		$this->client->followRedirect();
+		$this->client->submitForm('Se connecter', [
+			'_username' => 'user_with_role_user',
+			'_password' => '1234'
+		]);
+		$this->client->followRedirect();
+		$this->assertResponseIsSuccessful();
+
+		$user = $entityManager->getRepository(User::class)->findBy(['username' => 'user_with_role_user']);
+		$nbTasksBeforeDelete = count($entityManager->getRepository(Task::class)->findBy(['user' => $user]));
+
+		$this->client->request('GET', '/tasks');
+
+		$this->client->submitForm('Supprimer');
+
+		$this->client->followRedirect();
+		$this->assertResponseIsSuccessful();
+		$nbTasksAfterDelete = count($entityManager->getRepository(Task::class)->findBy(['user' => $user]));
+
+
+		$this->assertNotEquals($nbTasksBeforeDelete, $nbTasksAfterDelete);
 
 	}
 
