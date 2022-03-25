b48052ea7490994a495b53fe673e6ce2e0cd5904
Mickael Moley <mickael.moley@outlook.com>
2022-03-25 13:12:31 +0100

    Add codeCoverage ignore for path - it will not be executed by symfony

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -28,6 +28,7 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @codeCoverageIgnore*
      * @Route("/login_check", name="login_check")
      */
     public function loginCheck()
@@ -36,6 +37,7 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @codeCoverageIgnore*
      * @Route("/logout", name="logout")
      */
     public function logoutCheck()
